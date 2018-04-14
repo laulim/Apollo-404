9cf9cef5a929e09c5437d7b31efcebf6c9592ba7
Lyudmila <laudylim@yandex.ru>
2018-04-14 15:10:26 +0300

    Navi is ready

---
+++ js/main.js
@@ -0,0 +1,28 @@
+$(document).ready(function() {
+
+//Скрипт для показа и скрытия мобильного меню
+//Создает переменные для кнопки и для меню
+	var pull = $("#navigation-toggle");
+	var nav = $(".navigation ul");
+
+	//Описываем событие при нажатии на кнопку
+	$(pull).on("click", function(e){
+
+		// Отменяем стандартное поведение ссылки в браузере
+		e.preventDefault();
+
+		// Открываем/скрываем меню
+		$(nav).slideToggle();
+
+		//Добавляем модификатор --active
+		$(this).toggleClass('navigation__toggle-button--active');
+	});
+
+	// При изменении размера окна в большую сторону, если меню было скрыто, показываем его
+	$(window).resize(function(){
+		var w = $(window).width();
+		if(w > 720 && nav.is(':hidden')) {
+			nav.removeAttr('style');
+		}
+	});
+});
